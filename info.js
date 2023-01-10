d6866a563e99b03aa1a51cda072f0923d7092e33
hellboy41785 <unknown41785@gmail.com>
2023-01-10 23:45:04 +0530

    modified:   info.js 	modified:   package-lock.json 	modified:   package.json

--- info.js
+++ info.js
@@ -1,6 +1,5 @@
 const { myData, bankNifty } = require("./stocks");
 const schedule = require("node-schedule");
-const fetch = require('cross-fetch');
 
 const fetchData = (stockName, saveName) => {
   const stocks = {
@@ -14,121 +13,112 @@ const fetchData = (stockName, saveName) => {
   const myStock = new saveName(stocks);
   // myStock.save();
 
-
   const setData = async () => {
-    try {
-      const response = await fetch(
-        `https://www.nseindia.com/api/option-chain-indices?symbol=${stockName}`
-      );
-      const data = await response.json();
+    const response = await fetch(
+      `https://www.nseindia.com/api/option-chain-indices?symbol=${stockName}`
+    );
+    const data = await response.json();
 
-      const filtered = data.filtered.data;
+    const filtered = data.filtered.data;
 
-      const callChange = filtered.reduce(
-        (acc, current) => acc + current.CE?.changeinOpenInterest,
-        0
-      );
-      const putChange = filtered.reduce(
-        (acc, current) => acc + current.PE?.changeinOpenInterest,
-        0
-      );
+    const callChange = filtered.reduce(
+      (acc, current) => acc + current.CE?.changeinOpenInterest,
+      0
+    );
+    const putChange = filtered.reduce(
+      (acc, current) => acc + current.PE?.changeinOpenInterest,
+      0
+    );
 
-      const intraDayPCR = (putChange / callChange).toFixed(2);
+    const intraDayPCR = (putChange / callChange).toFixed(2);
 
-      const weightCall = filtered?.map((item, index) =>
-        item.CE.changeinOpenInterest === undefined
-          ? 1
-          : item.CE.changeinOpenInterest * item.CE.lastPrice
-      );
+    const weightCall = filtered?.map((item, index) =>
+      item.CE.changeinOpenInterest === undefined
+        ? 1
+        : item.CE.changeinOpenInterest * item.CE.lastPrice
+    );
 
-      const weightCallData = weightCall.reduce(
-        (acc, current) => acc + current,
-        0
-      );
+    const weightCallData = weightCall.reduce(
+      (acc, current) => acc + current,
+      0
+    );
 
-      const weightPut = filtered?.map(
-        (item, index) => item.PE.changeinOpenInterest * item.PE.lastPrice
-      );
+    const weightPut = filtered?.map(
+      (item, index) => item.PE.changeinOpenInterest * item.PE.lastPrice
+    );
 
-      const weightPutData = weightPut.reduce(
-        (acc, current) => acc + current,
-        0
-      );
+    const weightPutData = weightPut.reduce((acc, current) => acc + current, 0);
 
-      const weightAge = (weightPutData / weightCallData).toFixed(2);
+    const weightAge = (weightPutData / weightCallData).toFixed(2);
 
-      //Weighted PCR
+    //Weighted PCR
 
-      const weightedCall = filtered?.map(
-        (item, index) => item.CE.openInterest * item.CE.lastPrice
-      );
+    const weightedCall = filtered?.map(
+      (item, index) => item.CE.openInterest * item.CE.lastPrice
+    );
 
-      const weightedCallData = weightedCall.reduce(
-        (acc, current) => acc + current,
-        0
-      );
+    const weightedCallData = weightedCall.reduce(
+      (acc, current) => acc + current,
+      0
+    );
 
-      const weightedPut = filtered?.map(
-        (item, index) => item.PE.openInterest * item.PE.lastPrice
-      );
+    const weightedPut = filtered?.map(
+      (item, index) => item.PE.openInterest * item.PE.lastPrice
+    );
 
-      const weightedPutData = weightedPut.reduce(
-        (acc, current) => acc + current,
-        0
-      );
+    const weightedPutData = weightedPut.reduce(
+      (acc, current) => acc + current,
+      0
+    );
 
-      const weightedPCR = (weightedPutData / weightedCallData).toFixed(2);
+    const weightedPCR = (weightedPutData / weightedCallData).toFixed(2);
 
-      //Volume Weighted PCR
+    //Volume Weighted PCR
 
-      const volCall = filtered?.map(
-        (item, index) => item.CE.totalTradedVolume * item.CE.lastPrice
-      );
+    const volCall = filtered?.map(
+      (item, index) => item.CE.totalTradedVolume * item.CE.lastPrice
+    );
 
-      const volWeightedCall = volCall.reduce(
-        (acc, current) => acc + current,
-        0
-      );
-      const volPut = filtered?.map(
-        (item, index) => item.PE.totalTradedVolume * item.PE.lastPrice
-      );
+    const volWeightedCall = volCall.reduce((acc, current) => acc + current, 0);
+    const volPut = filtered?.map(
+      (item, index) => item.PE.totalTradedVolume * item.PE.lastPrice
+    );
 
-      const volWeightedPut = volPut.reduce((acc, current) => acc + current, 0);
+    const volWeightedPut = volPut.reduce((acc, current) => acc + current, 0);
 
-      const volWeightedPCR = (volWeightedPut / volWeightedCall).toFixed(2);
+    const volWeightedPCR = (volWeightedPut / volWeightedCall).toFixed(2);
 
-      stocks.i = [intraDayPCR, ...stocks.i];
-      stocks.w = [weightAge, ...stocks.w];
-      stocks.v = [volWeightedPCR, ...stocks.v];
-      stocks.wp = [weightedPCR, ...stocks.wp];
-      stocks.t = [Math.floor(Date.now() / 1000), ...stocks.t];
+    stocks.i = [intraDayPCR, ...stocks.i];
+    stocks.w = [weightAge, ...stocks.w];
+    stocks.v = [volWeightedPCR, ...stocks.v];
+    stocks.wp = [weightedPCR, ...stocks.wp];
+    stocks.t = [Math.floor(Date.now() / 1000), ...stocks.t];
 
-      const nifty = await saveName.findOneAndUpdate(
-        {},
-        { $set: stocks },
-        { new: true }
-      );
-    } catch (eerr) {
-      console.log(err);
-    }
+    const updateData = await saveName.findOneAndUpdate(
+      {},
+      { $set: stocks },
+      { new: true }
+    );
   };
 
   const rule = new schedule.RecurrenceRule();
   rule.dayOfWeek = new schedule.Range(1, 5);
-  rule.hour = 22;
-  rule.minute = 25;
+  rule.hour = 23;
+  rule.minute = 47;
   let intervalId;
   schedule.scheduleJob(rule, () => {
     console.log("Started data Collection");
     setData();
+    console.log(stocks);
     intervalId = setInterval(() => {
       setData();
+      console.log(stocks);
     }, 180000);
 
     const stopRule = new schedule.RecurrenceRule();
     stopRule.dayOfWeek = new schedule.Range(1, 5);
-    stopRule.hour = 22; // 4 PM
-    stopRule.minute = 30;
+    stopRule.hour = 23; // 4 PM
+    stopRule.minute = 60;
     const j = schedule.scheduleJob(stopRule, () => {
       console.log("Stoping data Collection");
       clearInterval(intervalId);
