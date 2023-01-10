2a1bcc74ef8c2a324d2a2c3f8582f621092d47e8
hellboy41785 <unknown41785@gmail.com>
2023-01-10 19:59:06 +0530

    modified:   info.js

--- info.js
+++ info.js
@@ -123,8 +123,8 @@ const fetchData = (stockName, saveName) => {
 
   const rule = new schedule.RecurrenceRule();
   rule.dayOfWeek = new schedule.Range(1, 5);
-  rule.hour = 19;
-  rule.minute = 50;
+  rule.hour = 20;
+  rule.minute = 02;
   let intervalId;
   schedule.scheduleJob(rule, () => {
     console.log("Started data Collection");
@@ -135,7 +135,7 @@ const fetchData = (stockName, saveName) => {
     const stopRule = new schedule.RecurrenceRule();
     stopRule.dayOfWeek = new schedule.Range(1, 5);
     stopRule.hour = 20; // 4 PM
-    stopRule.minute = 00;
+    stopRule.minute = 12;
     const j = schedule.scheduleJob(stopRule, () => {
       console.log("Stoping data Collection");
       clearInterval(intervalId);
