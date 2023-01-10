bbbf3aabcced2f9626eca22ab86101e07dfcbfd6
hellboy41785 <unknown41785@gmail.com>
2023-01-10 15:16:55 +0530

    modified:   app.js

--- app.js
+++ app.js
@@ -1,37 +1,57 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const  {myData,bankNifty}  = require("./stocks");
-const { nf,bn } = require("./info");
-const cors = require("cors")
+const { myData, bankNifty } = require("./stocks");
+const { nf, bn } = require("./info");
+const cors = require("cors");
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-const dbURI = process.env.MONGO_URI
+
+let date_time = new Date();
+
+let date = ("0" + date_time.getDate()).slice(-2);
+
+let month = ("0" + (date_time.getMonth() + 1)).slice(-2);
+
+let year = date_time.getFullYear();
+
+let hours = date_time.getHours();
+
+let minutes = date_time.getMinutes();
+
+let seconds = date_time.getSeconds();
+
+// console.log(
+//   year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds
+// );
+
+const currentTime =   year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds
+
+const dbURI = process.env.MONGO_URI;
 mongoose
   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => console.log("connected to db ......."))
   .catch((err) => console.log(err));
 
-
-
-app.get("/",  (req, res) => {
+app.get("/", (req, res) => {
   res.status(200).send({
+    currentTime: `${currentTime}`,
     message: "use this route",
     nifty: "/api/v1/NIFTY",
-    banknifty:"/api/v1/BANKNIFTY"
-  })
+    banknifty: "/api/v1/BANKNIFTY",
+  });
 });
-app.get("/api/v1/NIFTY",  async(req, res) => {
- const data = await myData.find({})
-  res.status(200).send(data)
+app.get("/api/v1/NIFTY", async (req, res) => {
+  const data = await myData.find({});
+  res.status(200).send(data);
 });
-app.get("/api/v1/BANKNIFTY",  async(req, res) => {
- const data = await bankNifty.find({})
-  res.status(200).send(data)
+app.get("/api/v1/BANKNIFTY", async (req, res) => {
+  const data = await bankNifty.find({});
+  res.status(200).send(data);
 });
 
-const PORT = process.env.PORT  || 5000
-app.listen(PORT,()=> console.log(`Server is running on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT} on time ${currentTime}`));
